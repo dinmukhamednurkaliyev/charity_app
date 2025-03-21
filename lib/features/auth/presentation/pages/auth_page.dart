eac9111c594d46c66ecebed57d86396db58d18d0
Dinmukhamed Nurkaliyev <dinmukhamed.nurkaliyev@gmail.com>
2025-03-21 08:08:11 +0500

    feat(auth): Implement initial UI for AuthPage with title and path

--- lib/features/auth/presentation/pages/auth_page.dart
+++ lib/features/auth/presentation/pages/auth_page.dart
@@ -3,8 +3,33 @@ import 'package:flutter/material.dart';
 class AuthPage extends StatelessWidget {
   const AuthPage({super.key});
 
+  static const String path = '/auth';
+
   @override
   Widget build(BuildContext context) {
-    return Scaffold();
+    return Scaffold(
+      body: SafeArea(
+        child: Padding(
+          padding: const EdgeInsets.symmetric(horizontal: 20),
+          child: Column(
+            children: [
+              Expanded(
+                child: Column(
+                  children: [
+                    Text(
+                      'Charity',
+                      style: Theme.of(context).textTheme.displayLarge?.copyWith(
+                        fontSize: 40,
+                        fontWeight: FontWeight.w900,
+                      ),
+                    ),
+                  ],
+                ),
+              ),
+            ],
+          ),
+        ),
+      ),
+    );
   }
 }
