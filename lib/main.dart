68bd43fb123ceece042274d15fe7716dac3ad4c9
Dinmukhamed Nurkaliyev <dinmukhamed.nurkaliyev@gmail.com>
2025-03-22 08:56:57 +0500

    feat: Integrate routing and theming in main.dart and update assets path

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
-import 'package:charity_app/features/splash/presentation/pages/splash_page.dart';
+import 'package:charity_app/core/router/app_router.dart';
+import 'package:charity_app/core/theme/app_theme.dart';
 import 'package:flutter/material.dart';
 import 'package:firebase_core/firebase_core.dart';
 import 'firebase_options.dart';
@@ -6,22 +7,10 @@ import 'firebase_options.dart';
 void main() async {
   WidgetsFlutterBinding.ensureInitialized();
   Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
-  runApp(const MyApp());
+  runApp(
+    MaterialApp.router(
+      routerConfig: AppRouter.router,
+      theme: AppTheme.getTheme(),
+    ),
+  );
 }
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      debugShowCheckedModeBanner: false,
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-      ),
-      home: SplashPage(),
-    );
-  }
-}
-
-
